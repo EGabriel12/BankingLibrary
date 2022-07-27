3dae9d2294260ec37903b3f81e965310ff13963f
Elias Gabriel <elias.gabrielfn12@gmail.com>
2022-07-27 19:50:06 -0400

    Create some tests

--- BankingLibrary.playground/Contents.swift
+++ BankingLibrary.playground/Contents.swift
@@ -1,17 +1,37 @@
 import Foundation
-
-let johnChecking = CheckingAccount()
-johnChecking.deposit(amount: 300.0)
-
-let check = johnChecking.writeCheck(amount: 200.0)!
-
-let janeChecking = CheckingAccount()
-janeChecking.deposit(check)
-janeChecking.balance
-
-janeChecking.deposit(check)
-janeChecking.balance
+import XCTest
 
 func createAccount() -> some Account {
     CheckingAccount()
 }
+
+class BankingTests: XCTestCase {
+    var checkingAccount: CheckingAccount!
+    
+    override func setUp() {
+        super.setUp()
+        checkingAccount = CheckingAccount()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        checkingAccount.withdraw(amount: checkingAccount.balance)
+    }
+    
+    func testNewAccountBalanceZero() {
+        XCTAssertEqual(checkingAccount.balance, 0.0)
+    }
+    
+    func testCheckOverBudgetFails() {
+        let check = checkingAccount.writeCheck(amount: 200.0)
+        XCTAssertNil(check)
+    }
+    
+    func testCheckIsCashed() {
+        checkingAccount.deposit(amount: 500)
+        let check = checkingAccount.writeCheck(amount: 200.0)
+        XCTAssertNotNil(check)
+    }
+}
+
+BankingTests.defaultTestSuite.run()
